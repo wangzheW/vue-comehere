ee08c2c40e9cce6728f34566f58db977ece930fe
wangzheW <1196784687@qq.com>
2017-11-01 15:57:26 +0800

    change

--- src/pages/home/Header.vue
+++ src/pages/home/Header.vue
@@ -1,19 +1,20 @@
 <template>
 	
 	<header class="header">
-		<a class="header-left" ><</a>
+		<a class="header-left iconfont">&#xe6bc;</a>
 		<div class="header-title" >
-			<span class="single-line">输入城市/景点/游玩主题..</span>			
+			<span class="single-line">输入城市/景点/游玩主题</span>			
 		</div>
 		<div class="header-right" >
-			<span class="header-city">北京</span>			
+			<span class="header-city">北京</span>
+			<span class="downarrow"></span>			
 		</div>
 	</header>
 
 </template>
 
 <script>
-
+import "../../assets/font/iconfont.css";
 
 export default {
 
@@ -28,10 +29,7 @@ export default {
 <style scoped>
 	.header{
 		position: relative;
-	    display: -webkit-box;
-	    display: -moz-box;
-	    display: -ms-flexbox;
-	    display: box;
+	    overflow: hidden;
 	    width: 100%;
 	    height: .88rem;
 	    background: #00bcd4;
@@ -39,27 +37,33 @@ export default {
 	    color: #fff;
 	}
 	.header-left{
-		display: inline-block;
+		float: left;
+	    display: block;
 	    width: .4rem;
 	    line-height: .88rem;
 	    padding: 0 .2rem;
+	    margin: .02rem 0 0 -0.08rem;
+	    margin-top: 0.02rem;
 	    color: #fff;
-	    font-size: .36rem;
+	    font-size: .40rem;
 	    text-align: left;
 	}
 	.header-title{
-	    -webkit-box-sizing: border-box;
-	    box-sizing: border-box;
-	    box-flex: 1;
-	    -webkit-box-flex: 1;
-	    -moz-box-flex: 1;
-	    -ms-flex: 1;
 	    position: relative;
 	    height: .6rem;
-	    margin: .14rem 0;
+	    margin: .14rem 1.32rem .14rem .8rem;
 	    background: #fff;
 	    line-height: .6rem;
 	    border-radius: .06rem;
+	}
+	.header-right{
+		position: absolute;
+		top: 0;
+		right: 0;
+		margin-right: .02rem;
+		line-height: .88rem;
+		width: 1.28rem;
+		text-align: center;
 	}
 	.single-line{
 		display: inline-block;
@@ -70,12 +74,25 @@ export default {
 	    width: 3.6rem;
 	    white-space: nowrap;
 	    text-overflow: ellipsis;
-	    color:#ccc;
+	    text-align: center;
+	    color: #ccc;
 	}
 	.header-city{
 		line-height: 0.88rem;
 		padding: 0 .22rem;
 		color: #fff;
+	}
+	.downarrow{
+		position: absolute;
+		top: 40%;
+		right: 0.08rem;
+		line-height: 0.88rem;
+		display: inline-block;
+	    width: 0;
+	    height: 0;
+	    border-left: 0.12rem solid transparent;
+	    border-right: 0.12rem solid transparent;
+	    border-top: 0.12rem solid #FFFFFF;
 	}
 </style>
 
