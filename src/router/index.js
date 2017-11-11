6d82ee3a92c2e675dd22effed49d351f66e84f80
jichaoW <565863188@qq.com>
2017-11-11 20:13:36 +0800

    wjcchange

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import List from '@/pages/list/index'
-
+import Onedaytour from '@/pages/onedaytour/index'
+import VueLazyload from 'vue-lazyload'
 Vue.use(Router)
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  attempt: 1
+})
 
 export default new Router({
   routes: [
@@ -15,6 +20,10 @@ export default new Router({
     	path:"/list",
     	name: 'List',
     	component: List
+    },{
+      path:"/onedaytour",
+      name: 'onedaytour',
+      component: Onedaytour
     }
   ]
 })
